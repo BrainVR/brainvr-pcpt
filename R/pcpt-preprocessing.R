8fc30a77c6f3e55bb955e88e8638f14aebc6ae1b
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-11-16 23:57:01 +0100

    Adds block trial number as well

--- R/pcpt-preprocessing.R
+++ R/pcpt-preprocessing.R
@@ -1,8 +1,18 @@
+#' Title
+#'
+#' @param df_pcpt
+#'
+#' @return
+#' @export
+#'
+#' @examples
 preprocess_experiment <- function(df_pcpt){
   df_pcpt$corr <- df_pcpt$corr == 1
   df_pcpt$responded <- df_pcpt$responded == 1
   df_pcpt$rt[df_pcpt$rt < 0] <- NA_integer_
   # add block trials as well
+  df_pcpt$block_trial <- as.vector(sapply(1:length(rle_res$lengths),
+                                          function(x){seq_len(rle_res$lengths[x])}))
 
   return(df_pcpt)
 }
