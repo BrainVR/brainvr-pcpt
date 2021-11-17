6caca45a4f0ebea722ab5d5c75d6d4c2363e12bb
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-11-17 01:51:56 +0100

    Adds analysis bias and sensivitiy

---
+++ R/pcpt-analysis.R
@@ -0,0 +1,52 @@
+#' Title
+#'
+#' @param df_pcpt preprocessed data frame with pebl data
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+analyze_performance <- function(df_pcpt){
+  res <- list()
+
+  res$n_trials <- nrow(df_pcpt)
+  res$n_targets <- sum(df_pcpt$should_respond)
+  res$n_foils <- sum(!df_pcpt$should_respond)
+  res$trial_accuracy <- sum(df_pcpt$corr)/res$n_trials
+  res$target_accuracy <- sum(df_pcpt$corr[df_pcpt$should_respond])/res$n_targets
+  res$foil_accuracy <- sum(df_pcpt$corr[!df_pcpt$should_respond])/res$n_foils
+
+  ## Reaction time
+  res$rt_mean <- mean(df_pcpt$rt, na.rm = TRUE)
+  res$rt_sd <- sd(df_pcpt$rt, na.rm = TRUE)
+  res$rt_correct_mean <- mean(df_pcpt$rt[df_pcpt$corr], na.rm=TRUE)
+  res$rt_correct_sd <- sd(df_pcpt$rt[df_pcpt$corr], na.rm=TRUE)
+  res$rt_error_mean <- mean(df_pcpt$rt[!df_pcpt$corr], na.rm=TRUE)
+  res$rt_error_sd <- sd(df_pcpt$rt[!df_pcpt$corr], na.rm=TRUE)
+
+  res$sensitivity_c <- sensitivity(res$target_accuracy, 1-res$foil_accuracy)
+  #res$beta_prime_d <- bias_beta_prime_d(res$target_accuracy, 1-res$foil_accuracy)
+  res$response_bias <- response_bias(res$target_accuracy, 1-res$foil_accuracy)
+  res <- as.data.frame(res)
+
+  return(res)
+}
+
+sensitivity <- function(hit_rate, false_alarm){
+  sens <- qnorm(hit_rate)-qnorm(false_alarm)
+  return(sens)
+}
+
+response_bias <- function(hit_rate, false_alarm){
+  #bias <- -0.5*(qnorm(hit_rate)^2-qnorm(false_alarm)^2)
+  # taken from https://github.com/neuropsychology/psycho.R/blob/master/R/dprime.R
+  bias <- exp((qnorm(false_alarm)^2/2)-(qnorm(hit_rate)^2/2))
+  return(bias)
+}
+
+bias_beta_prime_d <- function(hit_rate, false_alarm){
+  beta <- ((hit_rate * (1-hit_rate)) - (false_alarm* (1-false_alarm)))/
+    ((hit_rate * (1-hit_rate)) + (false_alarm* (1-false_alarm)))
+  return(beta)
+}
+
--- R/pcpt-preprocessing.R
+++ R/pcpt-preprocessing.R
@@ -1,8 +1,10 @@
-#' Title
+#' Preprocesses some of the columns int he loaded dataframe
 #'
-#' @param df_pcpt
+#' @description Converts column types, adds new columns
 #'
-#' @return
+#' @param df_pcpt data.frame loaded with `\code{\link{load_experiment}}`
+#'
+#' @return data.frame
 #' @export
 #'
 #' @examples
@@ -11,8 +13,11 @@ preprocess_experiment <- function(df_pcpt){
   df_pcpt$responded <- df_pcpt$responded == 1
   df_pcpt$rt[df_pcpt$rt < 0] <- NA_integer_
   # add block trials as well
+  rle_res <- rle(df_pcpt$block)
   df_pcpt$block_trial <- as.vector(sapply(1:length(rle_res$lengths),
                                           function(x){seq_len(rle_res$lengths[x])}))
-
+  df_pcpt$should_respond <- (df_pcpt$responded & df_pcpt$corr) |
+    (!df_pcpt$responded & !df_pcpt$corr)
   return(df_pcpt)
 }
+
