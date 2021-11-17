60358ece950f4f31d3c630f267d0123dada7f71f
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-11-17 12:24:39 +0100

    Fixes bug with loading

--- R/pcpt-loading.R
+++ R/pcpt-loading.R
@@ -1,14 +1,16 @@
 #' Loads experimental data from a folder. Expects two files _matr.csv
 #' and report _matr.txt.0
+#'
 #' @param folder Folder in which to search for the foiles.
+#' @param preprocess
 #'
 #' @return
 #'
-#'
+#' @export
 #' @examples
 load_experiment <- function(folder, preprocess = TRUE){
   ptr <- ".*\\.csv"
-  files <- list.files(TEST_PTH, ptr, full.names = TRUE)
+  files <- list.files(folder, ptr, full.names = TRUE)
   participant <- gsub("pcpt-(.*)\\.csv", "\\1", basename(files[1]))
   df_pcpt <- read.table(files[1], sep=",", header = TRUE)
   if(preprocess){
